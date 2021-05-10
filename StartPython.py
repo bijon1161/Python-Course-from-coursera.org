5f79497a11b18ec9e69e8ed537c89e92be712005
bijon <bijonndc@gmail.com>
2021-05-10 13:38:50 +0600

    Lecture 1 pre-exercise added

---
+++ StartPython.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+# -StartPython.py *- coding: utf-8 -*-
+"""
+Spyder Editor
+
+#%% starts a new cell. Use second green triangle to run just the cell that
+your mouse has last clicked in (or Ctrl-Enter on a PC or Command-Return on a
+Macintosh or Menu>Run>Run Cell)
+"""
+#%%
+def hello():
+    print("Hello, world!")
+
+#%%
+def myname():
+    print("My name is Bill")
+    
+#%%
+def ourschool():
+    print("Coursera is our school")
+
+#%%  
+  
+""" This will run forever. In this case Ctrl-C will stop it, but sometimes
+that doesn't work. In that case, click away IP Console to stop; click yes to 
+kill kernel. Open a new IPConsole on the Console menu to restart """
+#%%
+def forever():
+    while True:
+        pass
+  
+#%% 
+    
