adbf857821931d66f155d9af27732e9a0611f31b
bijon <bijonndc@gmail.com>
2021-05-14 20:59:09 +0600

    Problem2_2 solution added

--- ProblemSet2.py
+++ ProblemSet2.py
@@ -75,16 +75,16 @@ alist = ["a","e","i","o","u","y"]
 blist = ["alpha", "beta", "gamma", "delta", "epsilon", "eta", "theta"] 
 
 def problem2_2(my_list):
-    pass # replace this pass (a do-nothing) statement with your code
-
-
-
-
-
-
-
-
-
+    print(my_list)
+    print(my_list[0])
+    print(my_list[-1])
+    print(my_list[3:5])
+    print(my_list[:3])
+    print(my_list[3:])
+    print(len(my_list))
+    my_list.append("z")
+    print(my_list)
+    # replace this pass (a do-nothing) statement with your code
 
 
 #%%
