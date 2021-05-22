4ae0ff91b6ed5d444ed4432dffb0a679a8305eb6
bijon <bijonndc@gmail.com>
2021-05-22 14:39:27 +0600

    Problem3_3 solution added

--- ProblemSet3.py
+++ ProblemSet3.py
@@ -140,7 +140,9 @@ exactly. Everything you need to do this is covered in the lectures. ***
 def problem3_3(month, day, year):
     """ Takes date of form mm/dd/yyyy and writes it in form June 17, 2016 
         Example3_3: problem3_3(6, 17, 2016) gives June 17, 2016 """
-    pass # replace this pass (a do-nothing) statement with your code
+    months = ("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December")
+    print(months[month-1]+" "+str(day)+","+" "+str(year))
+        # replace this pass (a do-nothing) statement with your code
 
 #%%
 """
