8375501ef1b052b9120c7e6f88d6ac649bc02c11
bijon <bijonndc@gmail.com>
2021-06-01 20:45:37 +0600

    Problem4_3 added

--- ProblemSet4.py
+++ ProblemSet4.py
@@ -79,7 +79,8 @@ toothbrush               $  2.60
 def problem4_3(product, cost):
     """ Prints the product name in a space of 25 characters, left-justified
         and the price in a space of 6 characters, right-justified"""
-    pass # replace this pass (a do-nothing) statement with your code
+    print("{0:<25}${1:>6.2f}".format(product, cost))
+    # replace this pass (a do-nothing) statement with your code
 
 
 #%%    
